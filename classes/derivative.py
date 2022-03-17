cae906622773fb3a85c55b91d886b7c560d9da6e
Grayni <grayniweb@gmail.com>
2022-03-18 01:48:42 +0300

    used magic method __call__

---
+++ classes/derivative.py
@@ -0,0 +1,20 @@
+import math
+
+
+class Derivative:
+    def __init__(self, func):
+        self.__func = func
+
+    def __call__(self, x, dx=0.0001, *args, **kwargs):
+        return (self.__func(x + dx) - self.__func(x)) / dx
+
+
+def sin_func(value):
+    return math.sin(value)
+
+
+sin_func = Derivative(sin_func)
+
+df_sin_func = sin_func(math.pi/4)
+
+print(df_sin_func)
