191c095e82db4d938e4912bc862947810c7392cc
Grayni <grayniweb@gmail.com>
2022-03-17 19:47:00 +0300

    add verify_coord() # @staticmethod

--- classes/descriptor_1.py
+++ classes/descriptor_1.py
@@ -1,10 +1,16 @@
 class PositiveNumber:
+    @staticmethod
+    def verify_coord(coord):
+        if type(coord) != int:
+            raise ValueError('Введите целое число')
+        elif coord < 0:
+            raise ValueError('Значение должно быть больше нуля')
+
     def __get__(self, instance, owner):
         return getattr(instance, self.coord_name)
 
     def __set__(self, instance, value):
-        if value < 0:
-            raise ValueError('Значение должно быть больше нуля')
+        self.verify_coord(value)
         setattr(instance, self.coord_name, value)
 
     def __set_name__(self, owner, name):
@@ -26,5 +32,6 @@ class Coords:
 
 
 coords = Coords(4, 2, 9)
+coords.z = 20
 result = coords.show_coords()
 print(result)
