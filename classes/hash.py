8b6951378922fcc515352e01679cf495170781e5
Grayni <grayniweb@gmail.com>
2022-03-19 03:43:56 +0300

    __hash__ __eq__

---
+++ classes/hash.py
@@ -0,0 +1,20 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+p1 = Point(1, 4)
+p2 = Point(1, 4)
+
+
+d = {p1: 8, p2: 5}
+
+print(d)
+
