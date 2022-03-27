08ce14a44ffc5515b1ebf86496a880361bd7b84c
Grayni <grayniweb@gmail.com>
2022-03-27 04:08:47 +0300

    binary algorithm

---
+++ algorithms/binary_search.py
@@ -0,0 +1,29 @@
+lst = [4, 2, 54, 1, 6, 7, 9, 33, 22, 12, 5, 32]
+
+
+def binary_search(arr, value):
+    """
+    binary algorithm
+    :param arr: int values
+    :param value: need to find this value
+    :return: index of value in sort arr
+    """
+
+    arr.sort()
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        if arr[mid] == value:
+            return mid
+
+        if arr[mid] > value:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+print(binary_search(lst, 6))
+print(binary_search(lst, 16))
