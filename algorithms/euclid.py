328a3355b53c555cc447eeb2007233ddf84ca2c5
Grayni <grayniweb@gmail.com>
2022-03-31 23:57:22 +0300

    euclid's algorithm

---
+++ algorithms/euclid.py
@@ -0,0 +1,10 @@
+def algorithm_euclid(a, b):
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a + b
+
+
+print(algorithm_euclid(24, 18))
