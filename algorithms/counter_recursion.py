d3d6ea812ae41cb3457228a4820d3825520aad9a
Grayni <grayniweb@gmail.com>
2022-04-01 02:14:44 +0300

    шimprovement counter_recursion

--- algorithms/counter_recursion.py
+++ algorithms/counter_recursion.py
@@ -2,8 +2,7 @@ def counter_func(arr):
     if arr == []:
         return 0
     else:
-        arr.pop(0)
-        return 1 + counter_func(arr)
+        return 1 + counter_func(arr[1:])
 
 
 print(counter_func([1, 2, 3, 4, 5, 3, 3]))
