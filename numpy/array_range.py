02974ea54c513c14ab90a2a964f77b60f849ade9
Grayni <grayniweb@gmail.com>
2022-04-10 01:50:50 +0300

    arange, linspace, randint, random, random_sample

--- numpy/array_range.py
+++ numpy/array_range.py
@@ -3,8 +3,12 @@ import numpy as np
 arr1 = np.arange(0, 10, 1.5)  # fractional step
 print(*arr1)  # 0.0 1.5 3.0 4.5 6.0 7.5 9.0
 
+
 arr2 = np.linspace(0, 4, 5)  # range of numbers
 print(*arr2)  # 0.0 1.0 2.0 3.0 4.0
+
+arr3 = np.random.randint(0, 100, 10)
+print(arr3)  # 10 differet a  integers
 
 arr_random1 = np.random.random((5,))
 arr_random2 = np.random.random_sample((5,))
