699406c4a99a296a0433f3d68751bf767270c778
Grayni <grayniweb@gmail.com>
2022-04-10 17:24:22 +0300

    linalg.inv, flatten, trace, info

--- numpy/array_special.py
+++ numpy/array_special.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-arr = np.array([(1, 2, 3), (4, 5, 6), (7, 8, 9)], dtype=np.int64)
-
+arr = np.array([(1, 2, -1), (2, 5, -6), (3, 8, -10)], dtype=np.int64)
+print('matrix:')
+print(arr, '\n----\n')
+print('inverse matrix:')
+print(np.linalg.inv(arr), '\n----\n')  # inverse matrix
 arr1 = arr.T  # transposition
 print(arr, '\n----\n')
 print(arr1, '\n----\n')
