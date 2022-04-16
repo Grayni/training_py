6764cad3bc0845ca2dfc5387a356b19bcd123fda
Grayni <grayniweb@gmail.com>
2022-04-17 02:15:51 +0300

    find max and min number and subtraction

---
+++ examples/kaprekar_step.py
@@ -0,0 +1,9 @@
+def kaprekar_step(L):
+    min_num = int(''.join(map(str, sorted(L))))
+    max_num = int(''.join(map(str, sorted(L)[::-1])))
+    return max_num - min_num
+
+
+l = [4, 5, 6, 8]
+
+print(kaprekar_step(l))
