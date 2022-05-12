c195df3ffda352f74afa5f7f1a66072648c2c321
Grayni <grayniweb@gmail.com>
2022-05-13 02:20:57 +0300

    add border around matrix

--- numpy/matrix_border.py
+++ numpy/matrix_border.py
@@ -3,3 +3,9 @@ n, m = 3, 3
 Z = np.arange(n*m).reshape(n, m)
 Z = np.pad(Z, (1, 1), 'constant', constant_values=(0, 0))
 print(Z)
+
+print('')
+# Variant 2
+Z = np.arange(n*m).reshape(n, m)
+Z = np.pad(Z, 1, 'constant')
+print(Z)
