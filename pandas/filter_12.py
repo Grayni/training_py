31a188caa243f789e8d1a04d0bf30898d448e39f
Grayni <grayniweb@gmail.com>
2022-05-19 23:12:43 +0300

    change values in data

---
+++ pandas/filter_12.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({
+    'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+    'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+    'name': ['Murzik', 'Pushok', 'Kaa', 'Bobik', 'Strelka', 'Vaska', 'Kaa2', 'Murka', 'Graf', 'Muhtar'],
+    'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']},
+    index=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+)
+
+column = 'animal'
+old_value = 'snake'
+new_value = 'python'
+
+df[column] = df[column].map(lambda x: new_value if x == old_value else x)
+print(df)
+print('')
+
+
+# variant 2
+df = pd.DataFrame({
+    'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+    'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+    'name': ['Murzik', 'Pushok', 'Kaa', 'Bobik', 'Strelka', 'Vaska', 'Kaa2', 'Murka', 'Graf', 'Muhtar'],
+    'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']},
+    index=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+)
+print(df.replace({column: {old_value: new_value}}))
+print('')
+
+
+# variant 3
+df = pd.DataFrame({
+    'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+    'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+    'name': ['Murzik', 'Pushok', 'Kaa', 'Bobik', 'Strelka', 'Vaska', 'Kaa2', 'Murka', 'Graf', 'Muhtar'],
+    'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']},
+    index=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+)
+
+df.loc[df[column] == old_value, column] = new_value
+print(df)
