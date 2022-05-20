625dda40a1ece02b248d946455d6a6e713c77033
Grayni <grayniweb@gmail.com>
2022-05-20 22:06:16 +0300

    sort sizes

---
+++ pandas/02_warehouse.py
@@ -0,0 +1,11 @@
+# sort on sizes by stock of warehouse
+
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+data = pd.read_csv('./csv/torg.csv', sep=';').rename(columns={'IP_PROP32': 'size', 'CP_QUANTITY': 'quantity'})
+#val = data[['IP_PROP30', 'CP_QUANTITY']].groupby('IP_PROP30').sum()
+val = data[['size', 'quantity']].groupby('size').sum().sort_values('quantity')
+print(val)
