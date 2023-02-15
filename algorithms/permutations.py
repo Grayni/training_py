06d64a24350947ed4c01e3352f741dd6927689a4
Grayni <grayniweb@gmail.com>
2023-02-15 21:58:53 +0300

    permutations with yield

--- algorithms/permutations.py
+++ algorithms/permutations.py
@@ -18,5 +18,24 @@ def brackets_in(n):
 
 n = 3
 arr = set([''.join(element) for element in permutations(brackets_in(n))])
+#print(sorted(brackets_round(arr)))
 
-print(sorted(brackets_round(arr)))
+
+
+
+import itertools
+n = 3
+#print(sorted(list(set(['(' + ''.join(i) + ')' for i in itertools.permutations((n-1) * ['(', ')'])]))))
+
+
+def permutations(elements):
+    if len(elements) <= 1:
+        yield elements
+        return
+
+    for perm in permutations(elements[1:]):
+        for i in range(len(elements)):
+            yield perm[:i] + elements[0:1] + perm[i:]
+
+
+print(list(map(lambda x: ''.join(list(map(lambda l: ''.join(str(l)), x))), list(permutations([1, 2, 3])))))
