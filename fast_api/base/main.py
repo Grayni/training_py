0ee1584706476c2d0da4ac44d9ba96ef8d55b4dc
Grayni <grayniweb@gmail.com>
2024-02-04 00:47:46 +0300

    create class User

--- fast_api/base/main.py
+++ fast_api/base/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect, Form
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from models.User import User
 
 templates = Jinja2Templates(directory="templates")
 
@@ -16,6 +16,32 @@ app = FastAPI(
 )
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
+
+
+# Models
+user_1: User = User(id=1, name='John Doe', age=32)
+
+
+@app.get('/users')
+def showUser():
+    return user_1.model_dump()  # dict()
+
+
+# @app.post("/user")
+# async def show_user(usr: User):
+#     return {"name": usr.name,
+#             "age": usr.age,
+#             "is_adult": usr.age >= 18}
+
+
+@app.get('/user', response_class=HTMLResponse)
+def userFields(request: Request):
+    return templates.TemplateResponse('user.html', {'request': request, 'title': 'User'})
+
+
+@app.post('/user')
+def ageUser(name: str = Form(...), age: int = Form(ge=0, lt=111)):
+    return {'name': name, 'age': age, 'is_adult': age >= 18}
 
 
 @app.get('/', status_code=200, response_class=HTMLResponse)
